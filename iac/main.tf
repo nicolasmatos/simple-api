42ee5345e4dbeb08921a11456907a2496d27dcb0
Nicolas Matos <nicolasmatos0905@gmail.com>
2023-03-30 23:33:19 -0300

    IAC - Module RDS

--- iac/main.tf
+++ iac/main.tf
@@ -5,3 +5,15 @@ module "network" {
   project_name = var.project_name
   tags         = local.tags
 }
+
+module "rds" {
+  source = "./modules/rds"
+
+  aws_region       = var.aws_region
+  project_name     = var.project_name
+  db_group_private = module.network.db_group_private_id
+  sg_rds           = [module.network.sg_rds_id]
+  tags             = local.tags
+
+  depends_on = [module.network]
+}
