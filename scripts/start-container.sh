c2c3cefbfcd1acb9e2fad5d276e62543662e8704
Nicolas Matos <nicolasmatos0905@gmail.com>
2023-04-01 20:33:36 -0300

    Att build and deploy

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 455462641947.dkr.ecr.us-east-1.amazonaws.com
 docker run -itd -p 3000:3000 455462641947.dkr.ecr.us-east-1.amazonaws.com/kxc-desafio-tecnico:DEV
--- scripts/stop-container.sh
+++ scripts/stop-container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker rm -f $(docker ps -qa) || true
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $OWNER.dkr.ecr.$AWS_REGION.amazonaws.com
 docker rmi 455462641947.dkr.ecr.us-east-1.amazonaws.com/kxc-desafio-tecnico:DEV || true
