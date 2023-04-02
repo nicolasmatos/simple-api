c8cbea2a58d28266cf2726666eab278ca0cacf9c
Nicolas Matos <nicolasmatos0905@gmail.com>
2023-04-02 13:22:56 -0300

    Fix: Deploy

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 455462641947.dkr.ecr.us-east-1.amazonaws.com
+docker rm -f $(docker ps -qa) || true
 docker run -itd -p 3000:3000 455462641947.dkr.ecr.us-east-1.amazonaws.com/kxc-desafio-tecnico:DEV
